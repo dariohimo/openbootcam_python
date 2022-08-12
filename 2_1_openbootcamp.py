0fa2e8bb045f261435ca8616ec867fc331929dec
dariohimo <dariohimo@gmail.com>
2022-08-12 11:03:53 -0500

    Create 2_1_openbootcamp.py

---
+++ 2_1_openbootcamp.py
@@ -0,0 +1,10 @@
+""" 
+  openBootcamp
+  En este ejercicio tendrás que crear una variable a la cual 
+  asignarás una cadena de texto y la imprimirás por consola, a su vez,
+  tendrás que imprimir también el tipo de la variable por consola.
+"""
+
+una_Cadena_Texto = "Es una cadena de texto, y su tipo es: "
+
+print(f"\n{una_Cadena_Texto}\n\t\t\t\t\t'{type(una_Cadena_Texto)} ' ")
