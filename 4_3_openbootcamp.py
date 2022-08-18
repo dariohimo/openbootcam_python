12f2254fdb19ed3639574aa9f7e0beae56e8b7ec
dariohimo <dariohimo@gmail.com>
2022-08-17 19:12:23 -0500

    4_3_openbootcamp.py
    números del 1 al 100 en orden inverso

---
+++ 4_3_openbootcamp.py
@@ -0,0 +1,11 @@
+""" 
+Escribe un programa que sea capaz de mostrar 
+los números del 1 al 100 en orden inverso.
+"""
+
+print(f"###############################################\n\n\
+Números del 1 a 100 en inverso")
+
+
+for x in reversed(range(1, 101 )):
+    print(f"{x}")
